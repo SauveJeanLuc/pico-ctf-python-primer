f7157afefd6ab6d2b7536d74a975c678c8f0de85
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-28 18:13:41 +0000

    implemented function.py

---
+++ function.py
@@ -0,0 +1,16 @@
+# Function To Determine whether number is even or not
+def isEven(x):
+    if (x % 2 == 0):
+        return True
+    else:
+        return False
+
+
+# Ask user to enter a number
+print("Input a number:")
+my_number = input()
+
+if isEven(my_number):
+   print("The number is even")
+else:
+   print("The number is odd")
