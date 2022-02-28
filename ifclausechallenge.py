85bb8261966006f5c7bd640fdf76cf2bfe428e9a
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-28 17:17:04 +0000

    attempt to if clause challenge

---
+++ ifclausechallenge.py
@@ -0,0 +1,8 @@
+for i in range(100):
+    if i < 10:
+        print("The following number is less than 10")
+    elif (i > 10 &  i < 50):
+        print("The following number is between 10 and 50")
+    elif i > 50:
+        print("The following number is greater than 50")
+    print(i)
